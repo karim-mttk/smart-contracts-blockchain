a0b2f7f64ce08d8d7ebdff1655f5414db72ba822
karim_mttk <muttakin.karim@gmail.com>
2023-02-18 19:59:03 +0100

    new commits

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.8.8;
 
 contract SimpleStorage{
 
-uint256 favNum;
+    uint256 public favNum;
 
-function store(uint256 _favNum) public {
-    favNum = _favNum;
-}
+    function store(uint256 _favNum) public {
+        favNum = _favNum;
+        retrieve;
+    }
 
-
+    //view, pure
+    //view does not burn gas as it only reads data
+    function retrieve() public view returns(uint256){
+        return favNum;
+    }
 }

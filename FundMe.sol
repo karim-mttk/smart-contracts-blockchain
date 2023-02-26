22e484272b65ad8ead5770b3e6306deb3acefd56
karim_mttk <muttakin.karim@gmail.com>
2023-02-26 15:44:12 +0100

    new commit

--- FundMe.sol
+++ FundMe.sol
@@ -5,42 +5,27 @@ pragma solidity ^0.8.8;
 // withdraw funds
 // set min funding value in USD
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;    
+
     uint256 public minimumUSD = 50 * 1e18;
     //ethereum block chain takes  
 
     address[] public funders; 
     mapping(address => uint256) public addressToAmountFunded;
 
-
-
     function fund() public payable {
-        require(msg.value >= minimumUSD, "Didn't send enough!!");
+        //getConversionRate(msg.value);
+        //require(msg.value >= minimumUSD, "Didn't send enough!!");
+        require(msg.value.getConversionRate() >= minimumUSD, "Didn't send enough!!");
+        //msg.value.getConversionRate()
         // code to handle multiple funders
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value;
     }
-
-    function getPrice() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        (, int256 price, , , ) = priceFeed.latestRoundData();
-        return uint256(price * 1e10); // 1**10 == 10000000000
-    }
-
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
-        return priceFeed.version();
-    }
-
-    function getConversionRate(uint256 ethAmount) public view returns (uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
-
-        return ethAmountInUsd; 
-    }
-
     // add code to allow users to withdraw funds
     // function withdraw(){}
+    //the functions that deal with converting price has been moved to a seperate class in 
 }

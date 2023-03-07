8d15433cf6beb06ff6848f80d25a867983159633
Karim-Mttk <89412432+karim-mttk@users.noreply.github.com>
2023-03-07 19:36:43 +0100

    Add files via upload

--- FundMe.sol
+++ FundMe.sol
@@ -16,6 +16,14 @@ contract FundMe {
     address[] public funders; 
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+        
+
+    }
+
     function fund() public payable {
         //getConversionRate(msg.value);
         //require(msg.value >= minimumUSD, "Didn't send enough!!");
@@ -26,6 +34,41 @@ contract FundMe {
         addressToAmountFunded[msg.sender] = msg.value;
     }
     // add code to allow users to withdraw funds
-    // function withdraw(){}
+
     //the functions that deal with converting price has been moved to a seperate class in 
+
+    function withdraw() public onlyOwner {
+        //require(msg.sender == owner, "Sender not 'owner'!");
+       
+
+        for(uint256 funderIndex = 0; funderIndex<funders.length; funderIndex++){
+                address funder = funders[funderIndex];
+                addressToAmountFunded[funder] = 0;
+        }
+        //reset the array
+
+        funders = new address[](0);
+
+        //properly withdraw the funds
+
+        //3 different ways to move funds:
+
+        //transfer
+         payable(msg.sender).transfer(address(this).balance);
+
+        //send 
+        bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        require(sendSuccess, "Send failed");
+        //call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
+
+    }
+    //modifier
+    modifier onlyOwner{
+        require(msg.sender == owner, "Sender not 'owner'!");
+        _;
+    }
+
+    
 }

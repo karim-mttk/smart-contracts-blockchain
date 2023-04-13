31b188ad07f7186c336489786530d14bc2122ed5
karim-mttk <muttakin.karim@gmail.com>
2023-04-13 13:49:02 +0200

    updates project 3

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,16 @@
 // imports
 
+const { ethers } = require("hardhat");
+
 //async main
-async function main() {}
+async function main() {
+  const SimpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
+  console.log("**Deploying contract!** Pls wait...");
+  const simpleStorage = await SimpleStorageFactory.deploy();
+  await simpleStorage.deployed();
+  /*private key and RPC url is automatically set in hardhat.config*/
+  console.log(`Deployed to: ${simpleStorage.address}`);
+}
 
 //main
 main()
